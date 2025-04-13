dc30bfaded56d5c651d82c3ba6af41484b428773
wysocki-bu <wysockip@bu.edu>
2025-04-12 20:03:01 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,10 +1,24 @@
 import streamlit as st
 
-st.title("Page 3: Close Price data")
+st.title("Page 3: Original Code for Training Abnormal Accruals Model")
+
+# The original Colab Notebook Python Code can be found here:
+# http:
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+
+st.code(code, language="python")
+
+
+
+
+
+#st.title("Page 3: Close Price data")
 
 # Plot a Linbe Chart of the Stock Price of the stock
-if 'data' in st.session_state:
-   st.line_chart(st.session_state.data['Close'])
+#if 'data' in st.session_state:
+#   st.line_chart(st.session_state.data['Close'])
 
-else:
-   st.write("No stock data found. Please select stock on Main Page.")
+#else:
+#   st.write("No stock data found. Please select stock on Main Page.")
