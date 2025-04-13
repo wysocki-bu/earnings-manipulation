df2ca27eb3b18c13bbf6c1bba05d2270d7ad67fd
wysocki-bu <wysockip@bu.edu>
2025-04-12 21:28:09 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import pandas as pd
+import yfinance as yf
 
-st.title("Page 2: Stock Price Statistics")
+st.title("Page 2: Financials")
+
+aapl = yf.Ticker("AAPL")
+balance_sheet = aapl.balance_sheet
+#print(balance_sheet)
+
+st.write(balance_sheet)
 
 
-if 'data' in st.session_state:
-   st.write("Statistics of the DataFrame:")
-   st.write(st.session_state.data.describe())
-else:
-   st.write("No stock data found. Please select stock on Main Page.")
+#if 'data' in st.session_state:
+#   st.write("Statistics of the DataFrame:")
+#   st.write(st.session_state.data.describe())
+#else:
+#   st.write("No stock data found. Please select stock on Main Page.")
