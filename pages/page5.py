d9e9c9b08820f4cb0904662fb8f5c1a7b1ce19da
wysocki-bu <wysockip@bu.edu>
2025-04-13 09:47:08 -0400

    Update page5.py

--- pages/page5.py
+++ pages/page5.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Page 5: Code for this StreamLit App")
+st.title("5. Code for this StreamLit App")
 
 # The original Github repository for this app can be found here:
 # http:
@@ -12,5 +12,10 @@ with open('pages/page1.py', 'r') as file:
 
 st.subheader("Code for Page 2")
 with open('pages/page2.py', 'r') as file:
+            code = file.read()
+            st.code(code, language='python')
+
+st.subheader("Code for Page 3")
+with open('pages/page3.py', 'r') as file:
             code = file.read()
             st.code(code, language='python')
