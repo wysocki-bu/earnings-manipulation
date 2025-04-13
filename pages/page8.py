c86ed634e13c1dee28e8f5a7868916d4961d26cc
wysocki-bu <wysockip@bu.edu>
2025-04-13 16:25:30 -0400

    Create page8.py

---
+++ pages/page8.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import statsmodels.api as sm
+import pickle
+
+filename = 'ols_model.pkl'
+
+# Load the model
+loaded_model = pickle.load(open(filename, 'rb'))
+
+# Display the results in Streamlit
+st.title("Simple Linear Regression")
+
+# Display the model summary
+st.write("Model Summary:")
+st.write(loaded_model.summary())
+
