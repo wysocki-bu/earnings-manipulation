d237fdaea1ec42acf8fd5529aca2da494f00dc7a
wysocki-bu <wysockip@bu.edu>
2025-04-18 13:22:30 -0400

    Update page3-1.py

--- pages/page3-1.py
+++ pages/page3-1.py
@@ -43,7 +43,7 @@ with tabs[1]:
    st.write(df2)
 
 # Store element from df1 (value 5) into df2 at a specific location
-   df2.at[0,'inv_ta'] = balance_sheet.at['Total Assets'],1]
+   df2.loc[0,'inv_ta'] = balance_sheet.at['Total Assets'],1]
 #   st.write(df2)
    
 #   data = {'inv_ta': [(balance_sheet.loc[['Gross PPE'],1])/(balance_sheet.loc[['Total Assets'],1])]}
