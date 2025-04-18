6706e58d8953ba09e481a5fa90a3938dcc6b50c3
wysocki-bu <wysockip@bu.edu>
2025-04-18 12:58:58 -0400

    Update page3-1.py

--- pages/page3-1.py
+++ pages/page3-1.py
@@ -36,7 +36,7 @@ with tabs[0]:
 # Tab 2: Financial Ratios for Ticker
 with tabs[1]:
 
-   balance_sheet.columns = range(len(df.columns))
+   balance_sheet.columns = range(len(balance_sheet.columns))
    st.write(balance_sheet.loc[['Total Assets'],1])
    st.write(balance_sheet.loc[['Gross PPE'],0])
 
